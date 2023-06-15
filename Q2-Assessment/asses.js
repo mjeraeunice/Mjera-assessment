913ce8654c6218c9685d14df87860cd7b90f1d23
mjeraeunice <mjeraeunice@gmail.com>
2023-06-15 09:26:06 +0300

    worked on practical exercise on OOP

---
+++ Q2-Assessment/asses.js
@@ -0,0 +1,11 @@
+class Story{
+    constructor(title,moral_lessons,age_group){
+        this.title=title,
+        this.moral_lessons=moral_lessons,
+        this.age_group=age_group
+    }
+tellStory(length){
+ return 
+}
+}
+ 
